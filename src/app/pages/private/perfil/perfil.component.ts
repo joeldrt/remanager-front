fb8aecf4c74309deb5e836a3992447de712b0174
YisusFlores <jesushfloresn@gmail.com>
2018-07-22 00:41:29 -0500

    Trabajos sobre la página Perfil, pequeños cambios en el svgComponent

--- src/app/pages/private/perfil/perfil.component.ts
+++ src/app/pages/private/perfil/perfil.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../../../_services';
+import { User } from '../../../_models';
+
+// Services
+import { UserService } from '../../../_services';
 
 @Component({
   selector: 'app-perfil',
@@ -6,10 +11,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.component.scss']
 })
 export class PerfilComponent implements OnInit {
+  public user: User
+  public isEditing: boolean;
 
-  constructor() { }
+  constructor(
+    private accountService: AccountService,
+    private _userService: UserService
+  ) {
+
+  }
 
   ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem('account'));
+    if(!this.user) {
+      this.accountService.getAccount()
+        .subscribe(
+          user => {
+            this.user = user;
+          },
+          error => {
+            console.log(error);
+          });
+    }// end if
+  }// end - ngOnInit
+
+  // --- Functions
+
+  editForm(){
+    this.isEditing = true;
   }
+
+  cancelEdit(){
+    this.isEditing = false;
+    this.user = JSON.parse(localStorage.getItem('account'));
+  }
+
+  saveForm(){
+    this._userService.update(this.user).subscribe(
+      success => {
+        //debugger
+        this.user = <User> success.body;
+        localStorage.setItem('account', JSON.stringify(success.body));
+        console.log('Se actualizo la información --- ' + success);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }// end - saveForm
 
 }
