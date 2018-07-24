ec0ea8ea4c52b6990179508dcb21df509cbf7606
joe <joeldrt@gmail.com>
2018-07-23 21:38:56 -0500

    arreglo

--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpResponse} from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 import { User } from '../_models';
 import { environment } from '../../environments/environment';
@@ -18,15 +17,12 @@ export class AccountService {
     this.API_URL = environment.API_URL;
   }
 
-  getAccount(): Observable<User> {
-    return this.http.get<User>(this.API_URL + 'api/account')
-      .pipe(map(user => {
-        if (user) {
-          localStorage.setItem('account', JSON.stringify(user));
-        }
+  getAccount(): Observable<HttpResponse<User>> {
+    return this.http.get<User>(this.API_URL + 'api/account', { observe: 'response' });
+  }
 
-        return user;
-      }));
+  updateAccount(user:  User): Observable<HttpResponse<any>> {
+    return this.http.post<any>(this.API_URL + 'api/account', user, { observe: 'response' });
   }
 
 }
