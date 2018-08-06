5e874050e49215e2ee8b85a868e22e7c076f7f5e
joe <joeldrt@gmail.com>
2018-08-05 21:53:04 -0500

    Creando el objeto contrato
    El objeto contrato es capaz de ligar un cliente con un producto siempre siendo emitido por un vendedor, así mismo contiene pago real y pago programado para llevar ambos calendarios

---
+++ src/app/_models/contrato.ts
@@ -0,0 +1,28 @@
+
+export class PagoReal {
+  public fechaCreacion?: any; // No se llena, solo para lectura... llenado por el servidor
+  public monto?: number;
+  public correoQueValida?: string;
+  public validado?: boolean;
+  public archivos?: string[];
+}
+
+export class PagoProgramado {
+  public fechaCompromisoPago?: any;
+  public monto?: number;
+}
+
+export enum TipoContrato {
+  BLOQUEO, APARTADO, VENTA, DEVOLUCION
+}
+
+export class Contrato {
+  public fechaCreacion?: any; // No se llena, solo para lectura... llenado por el servidor
+  public tipo?: TipoContrato;
+  public clienteId?: string;
+  public productoId?: string;
+  public vendedorId?: string; // No se llena, solo para lectura... llenado por el servidor
+  public diasValidez?: number;
+  public pagosProgramados?: PagoProgramado[];
+  public pagosReales?: PagoReal[];
+}
