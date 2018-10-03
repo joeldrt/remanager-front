602371b045293ecd08d87702af9cd4cba076ba28
joe <joeldrt@gmail.com>
2018-10-02 23:52:26 -0500

    cambios para fix perfil component

--- src/app/_models/image.envelope.ts
+++ src/app/_models/image.envelope.ts
@@ -2,4 +2,10 @@ export class ImageEnvelope {
   public filename: string;
   public filetype: string;
   public value: string;
+
+  constructor(filename: string, filetype: string, value: string) {
+    this.filename = filename;
+    this.filetype = filetype;
+    this.value = value;
+  }
 }
